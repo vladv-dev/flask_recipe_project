ad7a9a97e04cc537145bc6c9fcc5ebb37fa7c68d
Vladimir Vecera <vlad.v@protonmail.com>
2022-10-13 19:27:56 +0100

    Added README file

--- app.py
+++ app.py
@@ -1,10 +1,13 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", methods = ["GET, POST"])
-def index():
-    return "its being built, don't worry!!"
+@app.route("/<name>")
+def index(name="Recipe Book"):
+    # not needed thanks to @app.route(/<name>):
+    #       name = request.args.get("name", name)
+    return "Hello from {}".format(name)
 
 
 app.run(debug=True, port=8000, host="0.0.0.0")
